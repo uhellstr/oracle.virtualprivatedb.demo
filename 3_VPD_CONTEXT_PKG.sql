83da13a64a9b1ddb002354cbf762b69afd163e5a
Ulf Hellström <ext.ulf.hellstrom@ehalsomyndigheten.se>
2017-06-09 09:46:52 +0200

    Minor changes of variable names

--- 3_VPD_CONTEXT_PKG.sql
+++ 3_VPD_CONTEXT_PKG.sql
@@ -10,34 +10,34 @@ END;
 
 CREATE OR REPLACE PACKAGE BODY vpd_test.set_vpd_session_context_pkg IS
   PROCEDURE set_context IS
-    v_ouser  VARCHAR2(30);
-    v_id     NUMBER;
+    lv_ouser  VARCHAR2(30);
+    lv_id     NUMBER;
   begin
     DBMS_SESSION.set_context('VPD_USER_CONTEXT','SETUP','TRUE');
-  
-    v_ouser := SYS_CONTEXT('USERENV','SESSION_USER');
-    
+
+    lv_ouser := SYS_CONTEXT('USERENV','SESSION_USER');
+
     -- Special handling for APEX
-    if v_ouser in ('ANONYMOUS','APEX_PUBLIC_USER') then
-      v_ouser := nvl(v('APP_USER'),USER);
+    if lv_ouser in ('ANONYMOUS','APEX_PUBLIC_USER') then
+      lv_ouser := nvl(v('APP_USER'),USER);
     end if;
-    
+
     begin
-    
+
       SELECT company_id
-      into   v_id
+      into   lv_id
       from   vpd_test.vpd_users
-      WHERE  username = v_ouser;
-      
-      dbms_session.set_context('VPD_USER_CONTEXT','USER_ID', v_id);
-      
+      WHERE  username = lv_ouser;
+
+      dbms_session.set_context('VPD_USER_CONTEXT','USER_ID', lv_id);
+
     EXCEPTION
       WHEN NO_DATA_FOUND THEN
         DBMS_SESSION.set_context('VPD_USER_CONTEXT','USER_ID', 0);
     END;
-    
+
     dbms_session.set_context('VPD_USER_CONTEXT','SETUP','FALSE');
-    
+
   end set_context;
 end set_vpd_session_context_pkg;
 /
